e13f5a2e50c1d03ab02c3ff03d895c484dc06256
RBinsonB <r-baran@hotmail.fr>
2021-02-22 18:27:38 +0100

    Add station access request to goto actions

--- nodes/ltl_automaton_nexus_node.py
+++ nodes/ltl_automaton_nexus_node.py
@@ -102,6 +102,9 @@ class LTLController(object):
         rospy.loginfo("LTL automaton Nexus node: waiting for nexus move base action server...")
         self.navigation.wait_for_server() # wait for action server to start
 
+        # Station access request publisher
+        self.station_access_request_pub = rospy.Publisher("station_access_request", std_msgs.msg.String, latch=True, queue_size=1)
+
         # Setup LTL state publisher
         self.ltl_state_pub = rospy.Publisher("ts_state", TransitionSystemStateStamped, latch=True, queue_size=10)
 
@@ -232,6 +235,12 @@ class LTLController(object):
             if region in self.occupied_regions:
                 # Region is already occupied, wait before moving
                 return False
+
+            # If next region is a station, request access, otherwise empty station request
+            station_access_req = std_msgs.msg.String()
+            if self.transition_system['2d_pose_region']['nodes'][region]['attr']['type'] == "station":
+                station_access_req.data = region
+            self.station_access_request_pub.publish(station_access_req)
 
             # Set new navigation goal and send
             GoalMsg = MoveBaseGoal()
